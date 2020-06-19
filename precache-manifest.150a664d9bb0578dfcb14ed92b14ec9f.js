7ceed45ff16772b22643f95f37e7860bb836fbc9
Sukruth M K <suk_mallur@yahoo.co.in>
2020-06-19 08:51:32 -0700

    Updates

---
+++ precache-manifest.150a664d9bb0578dfcb14ed92b14ec9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe55cb9b4848526bf727c1d4b55c47c2",
+    "url": "/sezzle-calc/index.html"
+  },
+  {
+    "revision": "9939ac1c3d5b965c4430",
+    "url": "/sezzle-calc/static/css/main.a59d3e2f.chunk.css"
+  },
+  {
+    "revision": "f5269dfaee5b5a0b5d0e",
+    "url": "/sezzle-calc/static/js/2.d26eb7b4.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/sezzle-calc/static/js/2.d26eb7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9939ac1c3d5b965c4430",
+    "url": "/sezzle-calc/static/js/main.272fafe2.chunk.js"
+  },
+  {
+    "revision": "a52c5d80592a8eb9d0ff",
+    "url": "/sezzle-calc/static/js/runtime-main.ee808bf4.js"
+  }
+]);
